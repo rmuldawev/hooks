7660f1ff284bbcbf28f44116c4b077b2a42ab514
rmuldawev <rmuldawev@gmail.com>
2022-12-14 20:58:09 +0500

    hooks

---
+++ src/examples/Ref.js
@@ -0,0 +1,34 @@
+import React, {useState, useEffect, useRef} from "react";
+
+let renderCount = 1
+
+function App() {
+  // const [renderCount, setRenderCount] = useState(1)
+  const [value, setValue] = useState('initial')
+  const renderCount = useRef(1)
+  const inputRef = useRef(null)
+  const prevValue = useRef('')
+
+  useEffect(() => {
+    renderCount.current++
+  })
+
+  useEffect(() => {
+    prevValue.current = value
+  }, [value])
+
+
+const focus = () => inputRef.current.focus()
+
+
+  return (
+    <div>
+      <h1>Количество рендеров: {renderCount.current}</h1>
+      <h2>Прошлое состояние: {prevValue.current}</h2>
+      <input ref={inputRef} type="text" onChange={e => setValue(e.target.value)} value={value}/>
+      <button className="btn btn-success" onClick={focus}>фокус</button>
+    </div>
+  )
+}
+
+export default App
